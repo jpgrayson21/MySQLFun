5744c283e3dfcf01c72f04d8e8e51c2c135ee735
James Grayson <jaygray@byu.edu>
2022-03-28 09:39:39 -0600

    Add project files.

---
+++ MySQLFun/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MySQLFun.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLFun.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRecipesRepository _repo { get; set; }
+
+        //constructor
+        public HomeController(IRecipesRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _repo.Recipes
+                .ToList();
+
+            return View(blah);
+        }
+    }
+}
---
+++ MySQLFun/Models/RecipesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLFun.Models
+{
+    public class RecipesDbContext : DbContext
+    {
+        public RecipesDbContext (DbContextOptions<RecipesDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
